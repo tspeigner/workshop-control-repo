aed601bf1533eca2b4d0d470feb5e115d1f781ed
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-12-14 16:10:31 +0100

    create workshop role - no content

---
+++ site/profile/manifests/workshop_classification.pp
@@ -0,0 +1,9 @@
+# This class configures PE node groups for the Bolt & PE Workshops
+#
+# @summary Configures PE node groups for the Bolt & PE Workshops
+#
+# @example
+#   include role::workshop
+class profile::workshop_classification {
+
+}
---
+++ site/role/manifests/workshop.pp
@@ -0,0 +1,9 @@
+# This class defines the Workshop role for PE
+#
+# @summary Defines the Workshop role for PE
+#
+# @example
+#   include role::workshop
+class role::workshop {
+  include profile::workshop_classification
+}

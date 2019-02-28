ee3e43e274f779a26c2a404178cb4b59746f6738
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-02-28 16:25:45 +0200

    Update win_packages_v2.pp

--- examples/win_packages_v2.pp
+++ examples/win_packages_v2.pp
@@ -2,7 +2,7 @@ class boltstudent::win_packages {
 
 Package {ensure => latest, provider => chocolatey,}
 
-$winpacks = lookup('profile::bolt_student::win_packages:winpacks')
+$winpacks = lookup('bolt_student::win_packages:winpacks')
 
   package { $winpacks: }
 }
